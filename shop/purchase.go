495c8a4fb41e12a495370eca2f83559d2f0ec447
Roy Brabson <rbrabson@gmail.com>
2025-03-03 03:15:22 -0500

    shop

---
+++ shop/purchase.go
@@ -0,0 +1,11 @@
+package shop
+
+type Purchase struct {
+}
+
+// TODO:
+// - this is used to keep track of purchases amde from the shop
+// - assign them to a guild member so the purchases can be displayed
+// - need to be able to see the status of the purchase
+//   - for instance, a custom message may take a while to get assigned
+//   - the status could be pending, approved, or denied
---
+++ shop/shop.go
@@ -0,0 +1,6 @@
+package shop
+
+type Shop struct{}
+
+type ShopItem struct {
+}
