4e513da17ce4906485c6a135558136a31dc843f3
Roy Brabson <rbrabson@gmail.com>
2025-03-03 03:15:22 -0500

    shop

---
+++ shop/message.go
@@ -0,0 +1,9 @@
+package shop
+
+type CustomMessage struct {
+}
+
+// TODO:
+// - give the ability to buy a custom message
+// - this needs to go through ModMail to get approved and created
+// - have a way to update the status
---
+++ shop/role.go
@@ -0,0 +1,7 @@
+package shop
+
+type Role struct {
+}
+
+// TODO:
+// - give ability to buy a role and have it assigned to the user
