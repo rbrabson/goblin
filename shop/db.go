da4bfbb847b339496c19224ad14117f17a49e516
Roy Brabson <rbrabson@gmail.com>
2025-03-04 13:32:48 -0500

    add shop

---
+++ shop/db.go
@@ -0,0 +1,13 @@
+package shop
+
+func readAllPurchasableItems(guildID string) ([]Purchasable, error) {
+	return nil, nil
+}
+
+func readAllPurchases(guildID string, memberID string) ([]*Purchase, error) {
+	return nil, nil
+}
+
+func writePurchase(purchase *Purchase) error {
+	return nil
+}
--- shop/purchase.go
+++ shop/purchase.go
@@ -1,11 +1,74 @@
 package shop
 
-type Purchase struct {
+import (
+	"time"
+
+	log "github.com/sirupsen/logrus"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+const (
+	PURCHASED = "purchased"
+	PENDING   = "pending"
+	APPROVED  = "approved"
+	DENIED    = "denied"
+)
+
+// Purchasable is an interface that represents an item that can be purchased from the shop.
+type Purchasable interface {
+	GetID() primitive.ObjectID
+	GetName() string
+	GetPrice() int
+	GetDescription() string
+	Buy(string, string) (*Purchase, error)
 }
 
-// TODO:
-// - this is used to keep track of purchases amde from the shop
-// - assign them to a guild member so the purchases can be displayed
-// - need to be able to see the status of the purchase
-//   - for instance, a custom message may take a while to get assigned
-//   - the status could be pending, approved, or denied
+// Purchase is a purchase made from the shop.
+type Purchase struct {
+	ID          primitive.ObjectID `json:"_id" bson:"_id"`
+	GuildID     string             `json:"guildID" bson:"guildID"`
+	MemberID    string             `json:"memberID" bson:"memberID"`
+	Purchasable Purchasable        `json:"purchasable" bson:"purchasable"`
+	Status      string             `json:"status" bson:"status"`
+	Date        time.Time          `json:"date" bson:"date"`
+}
+
+// GetAllPurchasableItems returns all items that may be purchased in the shop.
+func GetAllPurchasableItems(guildID string) []Purchasable {
+	log.Trace("--> shop.GetAllPurchasableItems")
+	defer log.Trace("<-- shop.GetAllPurchasableItems")
+
+	shopItems, err := readAllPurchasableItems(guildID)
+	if err != nil {
+		log.WithFields(log.Fields{"guild": guildID, "error": err}).Error("unable to read purchasable items from the database")
+		return nil
+	}
+	return shopItems
+}
+
+// GetAllRoles returns all the purchases made by a member in the guild.
+func GetAllPurchases(guildID string, memberID string) []*Purchase {
+	log.Trace("--> shop.GetAllPurchases")
+	defer log.Trace("<-- shop.GetAllPurchases")
+
+	purchases, err := readAllPurchases(guildID, memberID)
+	if err != nil {
+		log.WithFields(log.Fields{"guild": guildID, "member": memberID, "error": err}).Error("unable to read purchases from the database")
+		return nil
+	}
+
+	return purchases
+}
+
+// NewPurchase creates a new Purchase with the given guild ID, member ID, and Purchasable.
+func NewPurchase(guildID, memberID string, purchasable Purchasable) (*Purchase, error) {
+	purchase := &Purchase{
+		GuildID:     guildID,
+		MemberID:    memberID,
+		Purchasable: purchasable,
+		Status:      PENDING,
+		Date:        time.Now(),
+	}
+
+	return purchase, nil
+}
