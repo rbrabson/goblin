2e72bd245d40f20c652620384b75264178221533
Roy Brabson <rbrabson@gmail.com>
2025-03-04 14:01:33 -0500

    add shop

--- shop/role.go
+++ shop/role.go
@@ -9,12 +9,7 @@ type Role ShopItem
 
 // NewRole returns a new role that can be purchased in the shop
 func NewRole(guildID string, name string, price int, description string) *ShopItem {
-	return &ShopItem{
-		Name:        name,
-		Price:       price,
-		Description: description,
-		Type:        ROLE_TYPE,
-	}
+	return NewShopItem(guildID, name, description, ROLE_TYPE, price)
 }
 
 func (r *Role) Buy(guildID, memberID string) (*Purchase, error) {
@@ -23,6 +18,7 @@ func (r *Role) Buy(guildID, memberID string) (*Purchase, error) {
 	// TODO: verify the user hasn't already bought the same exact item
 	//       in the shop. For instance, don't buy a role if you already
 	//       have it.
+	// TODO: assign the role to the user in the guild.
 	item := ShopItem(*r)
 	return NewPurchase(guildID, memberID, &item)
 }
--- shop/shop.go
+++ shop/shop.go
@@ -17,9 +17,29 @@ type ShopItem struct {
 	Price       int                `json:"price" bson:"price"`
 }
 
+// GetShopItem returns the shop item with the given guild ID, name, and type.
 func GetShopItem(guildID string, name string, itemType string) *ShopItem {
 	// TODO: read from the DB, using "guildID, name, type as the keys"
 	return nil
+}
+
+// GetAllShopItems returns all the shop items for the given guild ID.
+func GetAllShopItems(guildID string) []*ShopItem {
+	// TODO: read from the DB, using "guildID" as the key
+	return nil
+}
+
+// NewShopItem creates a new ShopItem with the given guild ID, name, description, type, and price.
+func NewShopItem(guildID string, name string, description string, itemType string, price int) *ShopItem {
+	// TODO: write to the DB, but verify it is a unique item (or simply update it if it already exists)
+	//       the DB key should be guidID, name, and type.
+	return &ShopItem{
+		GuildID:     guildID,
+		Name:        name,
+		Description: description,
+		Type:        itemType,
+		Price:       price,
+	}
 }
 
 // String returns a string representation of the Role.
