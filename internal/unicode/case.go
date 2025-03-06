44a7368b511c68a84257593f98302af7f50f52a3
Roy Brabson <rbrabson@gmail.com>
2025-03-05 23:50:44 -0500

    adjust case of messages

---
+++ internal/unicode/case.go
@@ -0,0 +1,32 @@
+package unicode
+
+import (
+	"unicode"
+	"unicode/utf8"
+)
+
+// FirstToLower makes= the first character in a unicode string lower case.
+func FirstToLower(s string) string {
+	r, size := utf8.DecodeRuneInString(s)
+	if r == utf8.RuneError && size <= 1 {
+		return s
+	}
+	lc := unicode.ToLower(r)
+	if r == lc {
+		return s
+	}
+	return string(lc) + s[size:]
+}
+
+// FistToUpper makes= the first character in a unicode string upper case.
+func FirstToUpper(s string) string {
+	r, size := utf8.DecodeRuneInString(s)
+	if r == utf8.RuneError && size <= 1 {
+		return s
+	}
+	lc := unicode.ToUpper(r)
+	if r == lc {
+		return s
+	}
+	return string(lc) + s[size:]
+}
