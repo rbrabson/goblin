587343a9cb690916a842ea778de838c15a111f71
Roy Brabson <rbrabson@gmail.com>
2025-03-06 12:31:47 -0500

    add shop items

--- shop/commands.go
+++ shop/commands.go
@@ -19,67 +19,117 @@ var (
 			Description: "Commands used to interact with the shop for this server.",
 			Options: []*discordgo.ApplicationCommandOption{
 				{
-					Name:        "account",
-					Description: "Sets the amount of credits for a given member.",
-					Type:        discordgo.ApplicationCommandOptionSubCommand,
+					Name:        "add",
+					Description: "Adds an item to the shop that may be purchased by a member.",
+					Type:        discordgo.ApplicationCommandOptionSubCommandGroup,
 					Options: []*discordgo.ApplicationCommandOption{
 						{
-							Type:        discordgo.ApplicationCommandOptionString,
-							Name:        "id",
-							Description: "The member ID.",
-							Required:    true,
-						},
-						{
-							Type:        discordgo.ApplicationCommandOptionInteger,
-							Name:        "amount",
-							Description: "The amount to set the account to.",
-							Required:    true,
+							Name:        "role",
+							Description: "Adds a purchasable role to the shop.",
+							Type:        discordgo.ApplicationCommandOptionSubCommand,
+							Options: []*discordgo.ApplicationCommandOption{
+								{
+									Type:        discordgo.ApplicationCommandOptionString,
+									Name:        "name",
+									Description: "The name of the role that may be purchased.",
+									Required:    true,
+								},
+								{
+									Type:        discordgo.ApplicationCommandOptionString,
+									Name:        "description",
+									Description: "A brief description of the role to be purchased. This defaults to the role name",
+									Required:    false,
+								},
+								{
+									Type:        discordgo.ApplicationCommandOptionInteger,
+									Name:        "cost",
+									Description: "The cost of the role.",
+									Required:    true,
+								},
+								{
+									Type:        discordgo.ApplicationCommandOptionInteger,
+									Name:        "duration",
+									Description: "The length the role is assigned before being automatically removed.",
+									Required:    false,
+								},
+								{
+									Type:        discordgo.ApplicationCommandOptionInteger,
+									Name:        "renewable",
+									Description: "Whether the member can auto-renew the role purchase when the duration expires. This has no effect unless duration is set to a value.",
+									Required:    false,
+								},
+							},
 						},
 					},
 				},
 				{
-					Name:        "balance",
-					Description: "Set the default balance for the bank for the server.",
-					Type:        discordgo.ApplicationCommandOptionSubCommand,
+					Name:        "update",
+					Description: "Updates an item in the shop that may be purchased by a member.",
+					Type:        discordgo.ApplicationCommandOptionSubCommandGroup,
 					Options: []*discordgo.ApplicationCommandOption{
 						{
-							Type:        discordgo.ApplicationCommandOptionString,
-							Name:        "value",
-							Description: "The the default balance for the bank for the server.",
-							Required:    true,
+							Name:        "role",
+							Description: "Updates a purchasable role to the shop.",
+							Type:        discordgo.ApplicationCommandOptionSubCommand,
+							Options: []*discordgo.ApplicationCommandOption{
+								{
+									Type:        discordgo.ApplicationCommandOptionString,
+									Name:        "name",
+									Description: "The name of the role that may be purchased.",
+									Required:    true,
+								},
+								{
+									Type:        discordgo.ApplicationCommandOptionString,
+									Name:        "description",
+									Description: "A brief description of the role to be purchased.",
+									Required:    true,
+								},
+								{
+									Type:        discordgo.ApplicationCommandOptionInteger,
+									Name:        "cost",
+									Description: "The cost of the role.",
+									Required:    true,
+								},
+								{
+									Type:        discordgo.ApplicationCommandOptionInteger,
+									Name:        "duration",
+									Description: "The length the role is assigned before being automatically removed.",
+									Required:    false,
+								},
+								{
+									Type:        discordgo.ApplicationCommandOptionInteger,
+									Name:        "renewable",
+									Description: "Whether the member can auto-renew the role purchase when the duration expires.",
+									Required:    false,
+								},
+							},
 						},
 					},
 				},
 				{
-					Name:        "name",
-					Description: "Set the name of the bank for the server.",
-					Type:        discordgo.ApplicationCommandOptionSubCommand,
+					Name:        "remove",
+					Description: "Removes an item from the shop.",
+					Type:        discordgo.ApplicationCommandOptionSubCommandGroup,
 					Options: []*discordgo.ApplicationCommandOption{
 						{
-							Type:        discordgo.ApplicationCommandOptionString,
-							Name:        "value",
-							Description: "The the name of the bank for the server.",
-							Required:    true,
+							Name:        "role",
+							Description: "A role in the shop that is to be removed.",
+							Type:        discordgo.ApplicationCommandOptionSubCommand,
+							Options: []*discordgo.ApplicationCommandOption{
+								{
+									Type:        discordgo.ApplicationCommandOptionString,
+									Name:        "name",
+									Description: "The name of the role to remove from the shop.",
+									Required:    true,
+								},
+							},
 						},
 					},
 				},
 				{
-					Name:        "currency",
-					Description: "Set the currency for the server.",
-					Type:        discordgo.ApplicationCommandOptionSubCommand,
-					Options: []*discordgo.ApplicationCommandOption{
-						{
-							Type:        discordgo.ApplicationCommandOptionString,
-							Name:        "value",
-							Description: "The currency to set for the server.",
-							Required:    true,
-						},
-					},
-				},
-				{
-					Name:        "info",
-					Description: "Get information about the banking system configuration.",
-					Type:        discordgo.ApplicationCommandOptionSubCommand,
+					Name:        "list",
+					Description: "Lists the items in the shop.",
+					Type:        discordgo.ApplicationCommandOptionSubCommandGroup,
 				},
 			},
 		},
@@ -87,13 +137,63 @@ var (
 
 	memberCommands = []*discordgo.ApplicationCommand{
 		{
-			Name:        "shop",
-			Description: "Commands used to interact with the shop for this server.",
 			Options: []*discordgo.ApplicationCommandOption{
 				{
-					Name:        "account",
-					Description: "Bank account balance for the member.",
-					Type:        discordgo.ApplicationCommandOptionSubCommand,
+					Name:        "buy",
+					Description: "Adds an item to the shop that may be purchased by a member.",
+					Type:        discordgo.ApplicationCommandOptionSubCommandGroup,
+					Options: []*discordgo.ApplicationCommandOption{
+						{
+							Name:        "role",
+							Description: "Buys a custom role for the server.",
+							Type:        discordgo.ApplicationCommandOptionSubCommand,
+							Options: []*discordgo.ApplicationCommandOption{
+								{
+									Type:        discordgo.ApplicationCommandOptionString,
+									Name:        "name",
+									Description: "The name of the role to be purchased.",
+									Required:    true,
+								},
+								{
+									Type:        discordgo.ApplicationCommandOptionInteger,
+									Name:        "renew",
+									Description: "Controls whether the role is auto-renewed when the purchase duration expires.",
+									Required:    false,
+								},
+							},
+						},
+					},
+				},
+				{
+					Name:        "update",
+					Description: "Changes the auto-renew status for a role that was previously purchased.",
+					Type:        discordgo.ApplicationCommandOptionSubCommandGroup,
+					Options: []*discordgo.ApplicationCommandOption{
+						{
+							Name:        "role",
+							Description: "Adds a purchasable role to the shop.",
+							Type:        discordgo.ApplicationCommandOptionSubCommand,
+							Options: []*discordgo.ApplicationCommandOption{
+								{
+									Type:        discordgo.ApplicationCommandOptionString,
+									Name:        "name",
+									Description: "The name of the role to be purchased.",
+									Required:    true,
+								},
+								{
+									Type:        discordgo.ApplicationCommandOptionInteger,
+									Name:        "renew",
+									Description: "Controls whether the role is auto-renewed when the purchase duration expires.",
+									Required:    false,
+								},
+							},
+						},
+					},
+				},
+				{
+					Name:        "list",
+					Description: "Lists the items in the shop that may be purchased.",
+					Type:        discordgo.ApplicationCommandOptionSubCommandGroup,
 				},
 			},
 		},
