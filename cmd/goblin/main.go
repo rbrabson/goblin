38045e241f8d20e273296a697792d79441236143
Roy Brabson <rbrabson@gmail.com>
2025-03-10 22:09:59 -0400

    add shop

--- cmd/goblin/main.go
+++ cmd/goblin/main.go
@@ -14,7 +14,6 @@ import (
 	"github.com/rbrabson/goblin/leaderboard"
 	"github.com/rbrabson/goblin/payday"
 	"github.com/rbrabson/goblin/role"
-	"github.com/rbrabson/goblin/shop"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -62,7 +61,7 @@ func main() {
 	payday.Start()
 	race.Start()
 	role.Start()
-	shop.Start()
+	// shop.Start()
 
 	bot := discord.NewBot(BotName, Version, Revision)
 	err = bot.Session.Open()
