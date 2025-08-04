7ca37cef3ef8c5178cd110a0e631992b684c3bca
Roy Brabson <rbrabson@gmail.com>
2025-08-03 22:32:35 -0400

    stats

--- stats/time.go
+++ stats/time.go
@@ -4,7 +4,7 @@ import "time"
 
 // today returns the current date with the time set to midnight.
 func today() time.Time {
-	now := time.Now()
+	now := time.Now().UTC()
 	year, month, day := now.Date()
 	return time.Date(year, month, day, 0, 0, 0, 0, now.Location())
 }
