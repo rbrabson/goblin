3aa7525ea8e569dead57862bc5b15e70ab71dcd9
Roy Brabson <rbrabson@gmail.com>
2025-08-05 09:11:45 -0400

    stats

--- stats/commands.go
+++ stats/commands.go
@@ -113,6 +113,93 @@ var (
 						},
 					},
 				},
+				{
+					Name:        "churn",
+					Description: "View player churn.",
+					Type:        discordgo.ApplicationCommandOptionSubCommand,
+					Options: []*discordgo.ApplicationCommandOption{
+						{
+							Name:        "game",
+							Description: "The game for which to determine the churn.",
+							Type:        discordgo.ApplicationCommandOptionString,
+							Required:    true,
+							Choices: []*discordgo.ApplicationCommandOptionChoice{
+								{
+									Name:  "Heist",
+									Value: Heist,
+								},
+								{
+									Name:  "Race",
+									Value: Race,
+								},
+							},
+						},
+						{
+							Name:        "after",
+							Description: "The time period to check the player churn.",
+							Type:        discordgo.ApplicationCommandOptionString,
+							Required:    true,
+							Choices: []*discordgo.ApplicationCommandOptionChoice{
+								{
+									Name:  "One Day",
+									Value: OneDay,
+								},
+								{
+									Name:  "One Week",
+									Value: OneWeek,
+								},
+								{
+									Name:  "Three Months",
+									Value: ThreeMonths,
+								},
+								{
+									Name:  "Six Months",
+									Value: SixMonths,
+								},
+								{
+									Name:  "Nine Months",
+									Value: NineMonths,
+								},
+								{
+									Name:  "Twelve Months",
+									Value: TwelveMonths,
+								},
+							},
+						},
+						{
+							Name:        "since",
+							Description: "The time period to check the player churn.",
+							Type:        discordgo.ApplicationCommandOptionString,
+							Required:    false,
+							Choices: []*discordgo.ApplicationCommandOptionChoice{
+								{
+									Name:  "Last Week",
+									Value: LastWeek,
+								},
+								{
+									Name:  "Last Month",
+									Value: LastMonth,
+								},
+								{
+									Name:  "Three Months Ago",
+									Value: ThreeMonthsAgo,
+								},
+								{
+									Name:  "Six Months Ago",
+									Value: SixMonthsAgo,
+								},
+								{
+									Name:  "Nine Months Ago",
+									Value: NineMonthsAgo,
+								},
+								{
+									Name:  "Twelve Months Ago",
+									Value: TwelveMonthsAgo,
+								},
+							},
+						},
+					},
+				},
 			},
 		},
 	}
@@ -150,6 +237,8 @@ func statsAdmin(s *discordgo.Session, i *discordgo.InteractionCreate) {
 	switch options[0].Name {
 	case "activity":
 		playerActivity(s, i)
+	case "churn":
+		playerChurn(s, i)
 	}
 }
 
@@ -250,14 +339,100 @@ func playerActivity(s *discordgo.Session, i *discordgo.InteractionCreate) {
 	}
 }
 
-// // Find players who played in the last 6 months but haven't played in 30 days
-// sixMonthsAgo := time.Now().AddDate(0, -6, 0)
-// churn, err := GetPlayerChurn("guild123", "heist", sixMonthsAgo, 30*24*time.Hour)
+func playerChurn(s *discordgo.Session, i *discordgo.InteractionCreate) {
+	p := message.NewPrinter(language.AmericanEnglish)
 
-// // Find players who played this year but haven't played in 2 weeks
-// startOfYear := time.Date(2024, 1, 1, 0, 0, 0, 0, time.UTC)
-// churn, err := GetPlayerChurn("guild123", "heist", startOfYear, 14*24*time.Hour)
+	var game, after, since string
+	options := i.ApplicationCommandData().Options[0].Options
+	for _, option := range options {
+		switch option.Name {
+		case "game":
+			game = option.StringValue()
+		case "after":
+			after = option.StringValue()
+		case "since":
+			since = option.StringValue()
+		}
+	}
 
-// // Find recent players (last month) who haven't played in a week
-// lastMonth := time.Now().AddDate(0, -1, 0)
-// churn, err := GetPlayerChurn("guild123", "heist", lastMonth, 7*24*time.Hour)
+	slog.Debug("Player churn command received",
+		slog.String("guild_id", i.GuildID),
+		slog.String("game", game),
+		slog.String("after", after),
+		slog.String("since", since),
+	)
+
+	duration := getDuration(after)
+	checkAfter := getTime(since)
+
+	// guildID := i.GuildID
+	guildID := "236523452230533121"
+	churn, err := GetPlayerChurn(guildID, game, checkAfter, duration)
+	if err != nil {
+		slog.Error("failed to get player churn",
+			slog.Any("error", err),
+		)
+		resp := disgomsg.NewResponse(
+			disgomsg.WithContent("Failed to get player churn: " + err.Error()),
+		)
+		if err := resp.SendEphemeral(s, i.Interaction); err != nil {
+			slog.Error("failed to send response",
+				slog.Any("error", err),
+			)
+		}
+	}
+	slog.Debug("Player churn retrieved",
+		slog.String("guild_id", i.GuildID),
+		slog.String("game", game),
+		slog.String("after", after),
+		slog.Time("check_after", checkAfter),
+		slog.Int("total_players", churn.ActivePlayers+churn.InactivePlayers),
+		slog.String("active_players", p.Sprintf("%d (%.2f%%)", churn.ActivePlayers, churn.ActivePercentage)),
+		slog.String("inactive_players", p.Sprintf("%d (%.2f%%)", churn.InactivePlayers, churn.InactivePercentage)),
+	)
+
+	embeds := []*discordgo.MessageEmbed{
+		{
+			Title:  "Player Churn for " + game,
+			Fields: []*discordgo.MessageEmbedField{},
+		},
+	}
+	embeds[0].Fields = append(embeds[0].Fields, &discordgo.MessageEmbedField{
+		Name:   "After",
+		Value:  timeToString(after),
+		Inline: false,
+	})
+	if since != "" {
+		embeds[0].Fields = append(embeds[0].Fields, &discordgo.MessageEmbedField{
+			Name:   "Since",
+			Value:  timeToString(since),
+			Inline: false,
+		})
+	}
+	embeds[0].Fields = append(embeds[0].Fields, &discordgo.MessageEmbedField{
+		Name:   "Total Players",
+		Value:  p.Sprintf("%d", churn.ActivePlayers+churn.InactivePlayers),
+		Inline: false,
+	})
+	embeds[0].Fields = append(embeds[0].Fields, &discordgo.MessageEmbedField{
+		Name:   "Active Players",
+		Value:  p.Sprintf("%d (%.2f%%)", churn.ActivePlayers, churn.ActivePercentage),
+		Inline: false,
+	})
+	embeds[0].Fields = append(embeds[0].Fields, &discordgo.MessageEmbedField{
+		Name:   "Inactive Players",
+		Value:  p.Sprintf("%d (%.2f%%)", churn.InactivePlayers, churn.InactivePercentage),
+		Inline: false,
+	})
+
+	resp := disgomsg.NewResponse(
+		disgomsg.WithEmbeds(embeds),
+	)
+
+	if err := resp.Send(s, i.Interaction); err != nil {
+		slog.Error("failed to send response",
+			slog.Any("error", err),
+		)
+	}
+
+}
